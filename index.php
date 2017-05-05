6fb734ccf79294986956962df44df21261cd0714
boka <web_masters_07@mail.ru>
2017-05-05 16:34:48 +0300

    strucktura3

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php require_once('includes/header.php'); ?>
-<?php require_once('includes/header-top.php'); ?>
-<?php require_once('includes/sidebar.php'); ?>
-<?php require_once('includes/tovar.php'); ?>
-<?php require_once('includes/pagination.php'); ?>
-<?php require_once('includes/footer_top.php'); ?>
-<?php require_once('includes/footer_bottom.php'); ?>
-
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+			<?php require_once('includes/sidebar.php'); ?>
+			<?php require_once('includes/tovar.php'); ?>
+			<?php require_once('includes/pagination.php'); ?>
+		</div>
+	</div>
+	<div class="n-footer">
+		<?php require_once('includes/footer_top.php'); ?>
+		<?php require_once('includes/footer_bottom.php'); ?>
+	</div>
+</div>
 </body>
 </html>
+
+
+
