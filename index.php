2db6dc2af012f20824d39f2323ebeba57f5061bc
boka <web_masters_07@mail.ru>
2017-05-05 16:23:10 +0300

    strucktura

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
-
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/header-top.php'); ?>
-<?php require_once('includes/equipment.php'); ?>
-<?php require_once('includes/product.php'); ?>
-<?php require_once('includes/about.php'); ?>
-<?php require_once('includes/last.php'); ?>
-<?php require_once('includes/extra.php'); ?>
-<?php require_once('includes/fast.php'); ?>
-<?php require_once('includes/footer.php'); ?>
+<?php require_once('includes/sidebar.php'); ?>
+<?php require_once('includes/tovar.php'); ?>
+<?php require_once('includes/pagination.php'); ?>
+<?php require_once('includes/footer_top.php'); ?>
+<?php require_once('includes/footer_bottom.php'); ?>
 
 </body>
 </html>
--- js/fonts.js
+++ js/fonts.js
@@ -1,6 +1,6 @@
 
- 		$(document).ready(function() {
- 			// $("head").append('<link href="https://fonts.googleapis.com/css?family=PT+Sans&amp;subset=cyrillic" rel="stylesheet">');
- 			$("head").append("<link href='//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css' rel='stylesheet'>");
- 		}); 
+ 		 $(document).ready(function() {
+ 		 	$("head").append('<link href="https://fonts.googleapis.com/css?family=Arsenal|Open+Sans:400,400i,600,600i,700,700i|PT+Serif:400,400i,700" rel="stylesheet">');
+ 		// 	$("head").append("<link href='//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css' rel='stylesheet'>");
+ 		 }); 
  		
