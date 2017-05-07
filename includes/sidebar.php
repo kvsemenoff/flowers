a5f1780240654a3593b8091db7e14421d946b9cf
boka <web_masters_07@mail.ru>
2017-05-07 10:57:49 +0300

    sidebar

---
+++ includes/sidebar.php
@@ -0,0 +1,41 @@
+<div class="col-md-3">
+	<div class="sidebar">
+		<p class="sidebar-sale sidebar-sale_view sidebar-sale_padding">
+			<span class="sidebar-sale__span1">СКИДКИ</span><br>
+			на цветы в коробках<br>
+			<span class="sidebar-sale__span2">до 50%</span>
+		</p>
+	</div>
+</div>
+<ul class="sidebar-menu">
+	<li><a href="#">Индивидуальный заказ</a></li>
+	<li><a href="#">Создать свой букет</a></li>
+	<li><a href="#">Букет сезона</a></li>
+
+	<p>Свадебная флористика</p>
+	<ul>
+		<li><a href="#">Букет невесты</a></li>
+		<li><a href="#">Свадебное оформление</a></li>
+	</ul>
+
+	<p>Флористическое оформление</p>
+	<ul>
+		<li><a href="#">Праздники</a></li>
+		<li><a href="#">Мероприятия</a></li>
+		<li><a href="#">Интерьерные композиции</a></li>
+	</ul>
+
+	<p>Траурная флористика</p>
+	<ul>
+		<li><a href="#">Траурные букеты</a></li>
+		<li><a href="#">Венки из живых цветов</a></li>
+		<li><a href="#">Оформление</a></li>
+	</ul>
+	
+	<p>Комнатные растения</p>
+	<ul>
+		<li><a href="#">Озеленение помещений</a></li>
+		<li><a href="#">Уход</a></li>
+		<li><a href="#">Композиции из комнатных<br>растений</a></li>
+	</ul>
+</ul>
