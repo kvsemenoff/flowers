38027b80d356dc89fec8db00f08c028712c1091a
engine77 <zaur.1kager@gmail.com>
2017-05-07 12:34:34 +0400

    all file

---
+++ includes/tovar.php
@@ -0,0 +1,127 @@
+<div class="container">
+	<div class="col-md-9 mycolmd">
+		<div class="tovar_block">
+			<h1 class="h1 tovar_block__caption">Все букеты</h1>
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower1.jpg" alt="">
+					<span class="tovar_block__sale">СКИДКА</span>
+					<span class="tovar_block__hit">ХИТ</span>					
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower2.jpg" alt="">					
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower3.jpg" alt="">					
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower4.jpg" alt="">					
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+			
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower5.jpg" alt="">				
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower6.jpg" alt="">
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+			
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower4.jpg" alt="">					
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+			
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower5.jpg" alt="">					
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+
+			<div class="tovar_block__item">
+				<div class="tovar_block__img">
+					<img src="img/flower6.jpg" alt="">					
+				</div>
+				<div class="tovar_block__text">
+					<h4 class="tovar_block__name">Драцента Джанет Крейг</h4>
+					<span class="tovar_block__oldprice">1 269 руб.</span>
+					<span class="tovar_block__newprice">2 269 руб.</span>
+					<div class="clearfix"></div>
+					<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</div>
