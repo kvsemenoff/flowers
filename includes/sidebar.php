7950b14f89eb86d7ed55438abf08ea905dc4de4b
boka <web_masters_07@mail.ru>
2017-05-07 11:47:09 +0300

    struktura

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -4,37 +4,37 @@
 		на цветы в коробках<br>
 		<span class="sidebar-sale__span2">до 50%</span>
 	</p>
-	<div class="sidebar-box sidebar-box">
+	<div class="sidebar-box sidebar-box_view">
 		<ul class="sidebar-menu">
-			<li><a href="#">Индивидуальный заказ</a></li>
-			<li><a href="#">Создать свой букет</a></li>
-			<li><a href="#">Букет сезона</a></li>
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Индивидуальный заказ</a></li>
+			<li class="sidebar-menu__item"><a href="#">Создать свой букет</a></li>
+			<li class="sidebar-menu__item"><a href="#">Букет сезона</a></li>
 		</ul>
 		<span>Свадебная флористика</span>
-		<ul>
-			<li><a href="#">Букет невесты</a></li>
-			<li><a href="#">Свадебное оформление</a></li>
+		<ul class="sidebar-menu">
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Букет невесты</a></li>
+			<li class="sidebar-menu__item"><a href="#">Свадебное оформление</a></li>
 		</ul>
 
 		<span>Флористическое оформление</span>
-		<ul>
-			<li><a href="#">Праздники</a></li>
-			<li><a href="#">Мероприятия</a></li>
-			<li><a href="#">Интерьерные композиции</a></li>
+		<ul class="sidebar-menu">
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Праздники</a></li>
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Мероприятия</a></li>
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Интерьерные композиции</a></li>
 		</ul>
 
 		<span>Траурная флористика</span>
-		<ul>
-			<li><a href="#">Траурные букеты</a></li>
-			<li><a href="#">Венки из живых цветов</a></li>
-			<li><a href="#">Оформление</a></li>
+		<ul class="sidebar-menu">
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Траурные букеты</a></li>
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Венки из живых цветов</a></li>
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Оформление</a></li>
 		</ul>
 
 		<span>Комнатные растения</span>
-		<ul>
-			<li><a href="#">Озеленение помещений</a></li>
-			<li><a href="#">Уход</a></li>
-			<li><a href="#">Композиции из комнатных<br>растений</a></li>
+		<ul class="sidebar-menu">
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Озеленение помещений</a></li>
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Уход</a></li>
+			<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Композиции из комнатных<br>растений</a></li>
 		</ul>
 
 	</div>
