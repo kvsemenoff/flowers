e4a8feeda5e9aa9059d31d48a6c4b0db41fcdcc2
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-09 20:00:06 +0600

    .cart.php 85%

--- cart2.php
+++ cart2.php
@@ -16,7 +16,7 @@
 					      </div>
 					      <div class="tabs-content">
 					        <div class="tab-content tab-0">
-					          <div class="tab-content__inner"Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Злых толку ручеек запятой продолжил великий приставка, прямо свою дорогу переулка ему продолжил щеке, маленький, языком ее своего буквенных речью?</div>
+					          <div class="tab-content__inner">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Злых толку ручеек запятой продолжил великий приставка, прямо свою дорогу переулка ему продолжил щеке, маленький, языком ее своего буквенных речью?</div>
 					        </div>
 					      </div>
 					      <div class="tabs-content">
@@ -81,6 +81,63 @@
 					    		</div>
 					    	</form>
 					    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="cart-order">
+				<div class="container">
+					<div class="h4-wrap">
+						<h4 class="h4">Закажите вместе с товаром</h4>
+					</div>
+					<div class="tovars-block">
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower2.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower3.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower4.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower5.jpg" alt="">				
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
 						</div>
 					</div>
 				</div>
