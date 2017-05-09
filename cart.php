f6d14d276ff709f568a39c6b37cd6e957588010b
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-09 23:09:51 +0600

    end cart.php

---
+++ cart.php
@@ -0,0 +1,386 @@
+<?php require_once('includes/header.php'); ?>
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+			<div class="container">
+				<div class="row">
+					
+					<div class="cart-inner">
+
+						<div class="breadcrumps-wrap">
+							<ul class="breadcrumps">
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg"></a><i>/</i></li>
+								<li class="breadcrumps__link"><a href="#">Цветы в коробках</a><i>/</i></li>
+								<li class="breadcrumps__link"><a href="#">Корзина</a><i>/</i></li>
+								<li class="breadcrumps__link"><span>Букет из хризантем</span></li>
+							</ul>
+						</div>
+						<div class="h1-wrap">
+							<h1 class="h1">Букет из хризантем </h1>
+						</div>
+						
+
+						<div class="cart__left_block">
+							<div class="cart__left_block__img">
+								<img src="img/sliderbig1.jpg" alt="">
+							</div>
+							<div class="js-owl-slidercart cart__sliderbox">
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/slidercart1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="cart__right_block">							
+							<div class="cart__compound">
+								<span class="cart__compoundtxt">Состав:</span>
+								<table class="cart__compound__table">
+									<tr>
+										<td>розы</td>
+										<td>—  21 шт</td>
+									</tr>
+									<tr>
+										<td>хризантемы</td>
+										<td>—  5 шт</td>
+									</tr>
+									<tr>
+										<td>гербера</td>
+										<td>—  27 шт</td>
+									</tr>
+								</table>
+							</div>
+							<div class="cart__characteristics">
+								<div class="cart__char__left">
+									<span>Размер:</span>
+									<span>Артикул:</span>
+									<span>Кол-во:</span>
+									<span>Цвет:</span>
+								</div>
+
+								<div class="cart__char__right">
+									<span class="cart__size">20 cм <b>40 cм</b></span>
+									<span class="cart__article">12 34567</span>
+									<div class="cart__char__right__number">
+										<span class="cart-button cart-button2">-</span>
+										<input type="text" value="1" disabled="disabled" id="x">  
+										<span class="cart-button cart-button3">+</span>
+									</div>
+									<div class="cart__char__right__colors">
+										<a href="#"><i></i></a>
+										<a href="#"><i></i></a>
+										<a href="#"><i></i></a>
+										<a href="#"><i></i></a>
+										<a href="#"><i></i></a>
+										<a href="#"><i></i></a>
+									</div>
+									<div class="cart__colortxt"><span>желтый</span></div>
+								</div>
+							</div>
+							<div class="cart__optiopn">
+								<span class="cart__optiopn__cap">Цена:</span>
+								<label class="radio cart__radio">
+									<input class="radio__hidden" type="radio" name="checkbox-test">
+									<span class="radio__custom cart__custom"></span>
+									<span class="radio__label cart__label">Уменьшенный</span>
+									<span class="cartoldprice">1 200 руб.</span>
+									<span class="cartnewprice">500 руб.</span>
+								</label>
+								<label class="radio cart__radio cart__radio_checked">
+									<input class="radio__hidden" type="radio" name="checkbox-test" checked="checked">
+									<span class="radio__custom cart__custom"></span>
+									<span class="radio__label cart__label">Стандартный</span>
+									<span class="cartoldprice">1 200 руб.</span>
+									<span class="cartnewprice">900 руб.</span>
+								</label>
+								<label class="radio cart__radio">
+									<input class="radio__hidden" type="radio" name="checkbox-test">
+									<span class="radio__custom cart__custom"></span>
+									<span class="radio__label cart__label">Делюкс</span>
+									<span class="cartoldprice">1 200 руб.</span>
+									<span class="cartnewprice">1200 руб.</span>
+								</label>
+							</div>
+
+							<div class="cart__choise">
+								<span>Стандартный  — <b>1 200 руб.</b></span>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+
+						<div class="share_block">
+							<span class="share">Поделиться</span>
+							<div class="share__socials">
+								<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+								<script src="//yastatic.net/share2/share.js"></script>
+								<div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,moimir,gplus,viber,whatsapp,telegram"></div>
+							</div>
+						</div>
+
+						
+						<div class="cart__information">
+							<div class="cart__boxadd">
+								<div class="cart__boxadd__img">
+									<img src="img/infoimg1.png" alt="">
+								</div>
+								<div class="cart__boxadd__text">
+									<span class="cart__boxadd__hover">100% свежести</span>
+									<div class="cart__boxadd__show">
+										Доставка по Москве в пределах МКАД с 9:00 до 21:00—400 руб.
+										Доствка по Московкой области 
+										с 9:00 до 21:00—1000 руб. 
+									</div>
+								</div>
+							</div>
+							<div class="cart__boxadd">
+								<div class="cart__boxadd__img">
+									<img src="img/infoimg2.png" alt="">
+								</div>
+								<div class="cart__boxadd__text">
+									<span class="cart__boxadd__hover">Быстрая доставка</span>
+									<div class="cart__boxadd__show">
+										Доставка по Москве в пределах МКАД с 9:00 до 21:00—400 руб.
+										Доствка по Московкой области 
+										с 9:00 до 21:00—1000 руб. 
+									</div>
+								</div>
+							</div>
+							<div class="cart__boxadd">
+								<div class="cart__boxadd__img">
+									<img src="img/infoimg3.png" alt="">
+								</div>
+								<div class="cart__boxadd__text">
+									<span class="cart__boxadd__hover">Безопасная оплата</span>
+									<div class="cart__boxadd__show">
+										Доставка по Москве в пределах МКАД с 9:00 до 21:00—400 руб.
+										Доствка по Московкой области 
+										с 9:00 до 21:00—1000 руб. 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- part -2 -->
+						<div class="review-block">
+							<div class="tabs-block">
+					      <div class="tabs">
+					        <div class="tab tab-0">Описание</div>
+					        <div class="tab tab-1 active">Отзывы</div>
+					      </div>
+					      <div class="tabs-content">
+					        <div class="tab-content tab-0">
+					          <div class="tab-content__inner">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Злых толку ручеек запятой продолжил великий приставка, прямо свою дорогу переулка ему продолжил щеке, маленький, языком ее своего буквенных речью?</div>
+					        </div>
+					      </div>
+					      <div class="tabs-content">
+					        <div class="tab-content tab-1">
+						        <div class="tab-content__inner clearfix">
+						          <div class="tab-content__title">
+						          	<span class="review__name">Татьяна Иванова</span>
+						          	<span class="review__date">21.02.2017</span>
+						          </div>
+						          <div class="tab-content__text">
+						          	<p>Рыбные тексты также применяются для демонстрации различных видов шрифта и в разработке макетов. Как правило их содержание бессмысленно. По причине своей функции текста-заполнителя для макетов нечитабельность рыбных текстов имеет особое значение, так как человеческое восприятие имеет особенность, распознавать определенные образцы и повторения.</p>
+						          	<div class="review__raitings">
+						          		<div class="review__raiting review__raiting_like">
+						          			<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+						          			<i class="like-quant">0</i>
+					          			</div>
+						          		<div class="review__raiting review__raiting_dislike">
+						          			<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+						          			<i class="dislike-quant">0</i>
+						          		</div>
+						          	</div>
+						          </div>
+						        </div>
+					        </div>
+					      </div>
+					    </div>
+					    <div class="button-wrap">
+								<a href="#" class="button button_main button_review">Оставьте отзыв о товаре</a>
+					    	<a href="#" class="all-review"><span>Все отзывы (4)</span><i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+					    </div>
+					    <div class="take-review">
+					    	<span class="take-review__title">Оставить отзыв</span>
+					    	<form action="#">
+					    		<div class="form-inner clearfix">
+					    			<div class="input-wrap">
+						    			<span class="input-label">Ваше имя</span>
+											<div class="input input_theme_rosa">
+												<input type="text" class="input__control" name="uname" required>
+											</div> 
+						    		</div>
+						    		<div class="input-wrap input-wrap_right">
+						    			<span class="input-label">Ваш e-mail</span>
+											<div class="input input_theme_rosa">
+												<input type="email" class="input__control" name="email" required>
+											</div>
+						    		</div>
+						    		<div class="input-wrap big-wrap">
+						    			<textarea name="message" class="message message_theme_rosa"></textarea>
+						    		</div>
+						    		<div class="input-wrap big-wrap clearfix">
+						    			<div class="checkbox-wrap">
+						    				<label class="checkbox">
+													<input class="checkbox__hidden" type="checkbox" name="checkbox-test" checked="checked">
+													<span class="checkbox__custom"></span>
+													<span class="checkbox__label"> Я согласен на обработку моих <a href="#">персональных данных</a></span>
+												</label>
+						    			</div>
+											<div class="button-right">
+												<a href="#" class="button button_main button_mini">Далее</a>
+											</div>
+						    		</div>
+					    		</div>
+					    	</form>
+					    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="cart-tovars cart-tovars_grey">
+				<div class="container">
+					<div class="h4-wrap">
+						<h4 class="h4">Закажите с товаром</h4>
+					</div>
+					<div class="tovars-block">
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower2.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower3.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower4.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower5.jpg" alt="">				
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="cart-tovars cart-tovars_white">
+				<div class="container">
+					<div class="h4-wrap">
+						<h4 class="h4">Вы смотрели</h4>
+					</div>
+					<div class="tovars-block">
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower2.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower3.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower4.jpg" alt="">					
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+						<div class="tovar_block__item">
+							<div class="tovar_block__img">
+								<img src="img/flower5.jpg" alt="">				
+							</div>
+							<div class="tovar_block__text">
+								<a href="#" class="tovar_block__name">Драцента Джанет Крейг</a>
+								<span class="tovar_block__oldprice">1 269 руб.</span>
+								<span class="tovar_block__newprice">2 269 руб.</span>
+								<div class="clearfix"></div>
+								<a href="#" class="tovar_block__add_busket">В КОРЗИНУ</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="n-footer">
+		<?php require_once('includes/footer_top.php'); ?>
+		<?php require_once('includes/footer_bottom.php'); ?>
+	</div>
+</div>
+</body>
+</html>
+
+
+
