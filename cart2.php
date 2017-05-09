98343891b59606da5ae7588068f837239e3723fb
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-09 19:46:33 +0600

    cart2.php 70%

--- cart2.php
+++ cart2.php
@@ -16,16 +16,70 @@
 					      </div>
 					      <div class="tabs-content">
 					        <div class="tab-content tab-0">
-					          <h5>Debonair Style 3.75 carats Heart Shape  lorem upsum heart shape  lorem</h5>
-					          <p>Gemstones with a gorgeous deep rich color, vibrant luster and clean clarity."3.75 carats Heart Shape Created Blue Sapphire Pendant Earrings Set in Sterling Silver Earrings: Created Blue Sapphire: 2 pieces, Heart Shape, 6.00mm. White Cubic Zirconia: 10 pieces, Round Shape 2.00mm White Cubic Zirconia: 2 pieces, Round Shape</p>
+					          <div class="tab-content__inner"Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Злых толку ручеек запятой продолжил великий приставка, прямо свою дорогу переулка ему продолжил щеке, маленький, языком ее своего буквенных речью?</div>
 					        </div>
 					      </div>
 					      <div class="tabs-content">
 					        <div class="tab-content tab-1">
-					          <h5>Second tab list officia perspiciatis voluptates ex.</h5>
-					          <p>2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus earum suscipit quis voluptatem enim. Dicta reprehenderit fuga ducimus non in nisi adipisci sequi est? Corporis voluptate eaque facere possimus, fuga!</p>
+						        <div class="tab-content__inner clearfix">
+						          <div class="tab-content__title">
+						          	<span class="review__name">Татьяна Иванова</span>
+						          	<span class="review__date">21.02.2017</span>
+						          </div>
+						          <div class="tab-content__text">
+						          	<p>Рыбные тексты также применяются для демонстрации различных видов шрифта и в разработке макетов. Как правило их содержание бессмысленно. По причине своей функции текста-заполнителя для макетов нечитабельность рыбных текстов имеет особое значение, так как человеческое восприятие имеет особенность, распознавать определенные образцы и повторения.</p>
+						          	<div class="review__raitings">
+						          		<div class="review__raiting review__raiting_like">
+						          			<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+						          			<i class="like-quant">0</i>
+					          			</div>
+						          		<div class="review__raiting review__raiting_dislike">
+						          			<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+						          			<i class="dislike-quant">0</i>
+						          		</div>
+						          	</div>
+						          </div>
+						        </div>
 					        </div>
 					      </div>
+					    </div>
+					    <div class="button-wrap">
+								<a href="#" class="button button_main button_review">Оставьте отзыв о товаре</a>
+					    	<a href="#" class="all-review"><span>Все отзывы (4)</span><i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+					    </div>
+					    <div class="take-review">
+					    	<span class="take-review__title">Оставить отзыв</span>
+					    	<form action="#">
+					    		<div class="form-inner clearfix">
+					    			<div class="input-wrap">
+						    			<span class="input-label">Ваше имя</span>
+											<div class="input input_theme_rosa">
+												<input type="text" class="input__control" name="uname" required>
+											</div> 
+						    		</div>
+						    		<div class="input-wrap input-wrap_right">
+						    			<span class="input-label">Ваш e-mail</span>
+											<div class="input input_theme_rosa">
+												<input type="email" class="input__control" name="email" required>
+											</div>
+						    		</div>
+						    		<div class="input-wrap big-wrap">
+						    			<textarea name="message" class="message message_theme_rosa"></textarea>
+						    		</div>
+						    		<div class="input-wrap big-wrap clearfix">
+						    			<div class="checkbox-wrap">
+						    				<label class="checkbox">
+													<input class="checkbox__hidden" type="checkbox" name="checkbox-test" checked="checked">
+													<span class="checkbox__custom"></span>
+													<span class="checkbox__label"> Я согласен с <a href="#">условиями работы</a></span>
+												</label>
+						    			</div>
+											<div class="button-right">
+												<a href="#" class="button button_main button_mini">Далее</a>
+											</div>
+						    		</div>
+					    		</div>
+					    	</form>
 					    </div>
 						</div>
 					</div>
