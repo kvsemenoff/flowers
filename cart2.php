3eaa160a33349b3917acb2f66786a2d3c3d53414
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-09 17:59:37 +0600

    begin cart2.php

---
+++ cart2.php
@@ -0,0 +1,132 @@
+<?php require_once('includes/header.php'); ?>
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+			<div class="container">
+				<div class="row">
+					
+					<div class="cart-inner">
+						<div class="breadcrumps-wrap">
+							<ul class="breadcrumps">
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg"></a><i>/</i></li>
+								<li class="breadcrumps__link"><a href="#">Цветы в коробках</a><i>/</i></li>
+								<li class="breadcrumps__link"><a href="#">Корзина</a><i>/</i></li>
+								<li class="breadcrumps__link"><span>Букет из хризантем</span></li>
+							</ul>
+						</div>
+						<div class="h1-wrap">
+							<h1 class="h1">Букет из хризантем </h1>
+						</div>
+						
+
+						<div class="cart__left_block">
+							<div class="cart__left_block__img">
+								<img src="img/sliderbig1.jpg" alt="">
+							</div>
+							<div class="cart__slider js-owl-slidercart">
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/slidercart1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="cart__right_block">
+							
+							<div class="cart__compound">
+								<span class="cart__compoundtxt">Состав:</span>
+								<table class="cart__compound__table">
+									<tr>
+										<td>розы</td>
+										<td>—  21 шт</td>
+									</tr>
+									<tr>
+										<td>хризантемы</td>
+										<td>—  5 шт</td>
+									</tr>
+									<tr>
+										<td>гербера</td>
+										<td>—  27 шт</td>
+									</tr>
+								</table>
+							</div>
+							<div class="cart__characteristics">
+								<div class="cart__char__left">
+									<span>Размер:</span>
+									<span>Артикул:</span>
+									<span>Кол-во:</span>
+									<span>Цвет:</span>
+								</div>
+								<div class="cart__char__right">
+									<span>20 cм <b>40 cм</b></span>
+									<span>12 34567</span>
+									<div class="cart__char__right__number">
+										<span class="cart-button cart-button2">-</span>
+										<input type="text" value="1" disabled="disabled" id="x">  
+										<span class="cart-button cart-button3">+</span>
+									</div>
+									<div class="cart__char__right__colors">
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+									</div>
+								</div>
+							</div>
+
+
+
+						</div>
+
+
+
+
+
+
+
+
+
+
+
+
+					</div>
+
+
+
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="n-footer">
+		<?php require_once('includes/footer_top.php'); ?>
+		<?php require_once('includes/footer_bottom.php'); ?>
+	</div>
+</div>
+</body>
+</html>
+
+
+
