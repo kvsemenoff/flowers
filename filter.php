3d43d1bf31a6614b4ef34fde4a82ce3acb76ed40
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-09 11:18:15 +0600

    add services

---
+++ filter.php
@@ -0,0 +1,41 @@
+<?php require_once('includes/header.php'); ?>
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+			<div class="services">
+				<div class="container">
+					<div class="row">
+						<div class="services-top clearfix">
+							<div class="col-md-12">
+								<div class="breadcrumps-wrap">
+									<ul class="breadcrumps">
+										<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg"></a><i>/</i></li>
+										<li class="breadcrumps__link"><a href="#">Цветы в коробках</a><i>/</i></li>
+										<li class="breadcrumps__link"><span>О нас</span></li>
+									</ul>
+								</div>
+								<div class="h1-wrap">
+									<h1 class="h1">Букеты</h1>
+								</div>
+								<?php require_once('includes/tovar.php'); ?>
+								<?php require_once('includes/pagination.php'); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="n-footer">
+		<?php require_once('includes/footer_top.php'); ?>
+		<?php require_once('includes/footer_bottom.php'); ?>
+	</div>
+</div>
+</body>
+</html>
+
+
+
---
+++ services.php
@@ -0,0 +1,133 @@
+<?php require_once('includes/header.php'); ?>
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+			<div class="services">
+				<div class="container">
+					<div class="row">
+						<div class="services-top clearfix">
+							<div class="col-md-12">
+								<div class="breadcrumps-wrap">
+									<ul class="breadcrumps">
+										<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg"></a><i>/</i></li>
+										<li class="breadcrumps__link"><a href="#">Цветы в коробках</a><i>/</i></li>
+										<li class="breadcrumps__link"><span>Услуги</span></li>
+									</ul>
+								</div>
+								<div class="h1-wrap">
+									<h1 class="h1">Услуги</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row clearfix">
+						<div class="col-md-9 col-sm-8 col-xs-12">
+							<div class="services-wrap">
+								<div class="services-inner">
+									<div class="q-wrap">
+										<div class="q">
+											<p>Магазин «Цветы и мечты»  предлагает широкий выбор услуг по продаже живых цветов и формированию букетов. Опытные флористы с безупречным вкусом и высоким уровнем мастерства создадут для вас букеты и цветочные композиции на заказ, а также произведут упаковку подарков ручной работы, приобрести которые вы можете в нашем магазине. </p>
+										</div>
+									</div>
+									<div class="services-items">
+										<div class="services__item">
+											<div class="h4-wrap">
+												<h4 class="h4">Живые цветы и цветочные композиции</h4>
+											</div>
+											<div class="item clearfix">
+												<div class="item__img">
+													<img src="img/services-item-1.jpg" alt="">
+												</div>
+												<div class="item__text-wrap">
+													<p class="item__text">Флористика - основное направление нашей деятельности, и мы всегда готовы к выполнению профессиональных работ в этой области. Если вам необходим букет для особого дня или цветочная композиция для украшения помещения - вы всегда можете обратиться к нашим специалистам, и они, опираясь на ваши пожелания, разработают уникальный дизайн и создадут композицию, идеально подходящую поводу.</p>
+													<p class="item__text">Мы используем огромное множество разнообразных цветов и аксессуаров, что позволяет формировать композиции, наполненные особым смыслом и не похожие одна на другую. Главным для нас при создании нового букета является индивидуальность получателя - мы стремимся угадать характер человека и сделать такой букет, который бы обрадовал и удиви его.</p>
+												</div>
+											</div>
+										</div>
+										<div class="services__item">
+											<div class="h4-wrap">
+												<h4 class="h4">Подарки ручной работы</h4>
+											</div>
+											<div class="item clearfix">
+												<div class="item__img">
+													<img src="img/services-item-2.jpg" alt="">
+												</div>
+												<div class="item__text-wrap">
+													<p class="item__text">Цветы - не единственное, что вы можете купить в нашем магазине. Также мы продаём оригинальные и красивые подарки и игрушки ручной работы. Они, без сомнения, порадуют любого человека, будь то ребёнок или взрослый, став прекрасным воспоминанием и свидетельством нежных чувств и искреннего внимания. А праздничная упаковка, сделанная умелыми руками наших мастеров, увеличит их очарование в сто раз!</p>
+													<p class="item__text item__text_italic">В магазине действует сервис доставки, которым может воспользоваться каждый клиент. Просто позвоните нам, и мы сделаем всё, чтобы выполнить ваш заказ качественно и оперативно! </p>
+												</div>
+											</div>
+										</div>
+										<div class="services__item">
+											<div class="h4-wrap">
+												<h4 class="h4">Подарки ручной работы</h4>
+											</div>
+											<div class="item clearfix">
+												<div class="item__img">
+													<img src="img/services-item-3.jpg" alt="">
+												</div>
+												<div class="item__text-wrap">
+													<p class="item__text">Цветы - не единственное, что вы можете купить в нашем магазине. Также мы продаём оригинальные и красивые подарки и игрушки ручной работы. Они, без сомнения, порадуют любого человека, будь то ребёнок или взрослый, став прекрасным воспоминанием и свидетельством нежных чувств и искреннего внимания. А праздничная упаковка, сделанная умелыми руками наших мастеров, увеличит их очарование в сто раз!</p>
+													<p class="item__text item__text_italic">В магазине действует сервис доставки, которым может воспользоваться каждый клиент. Просто позвоните нам, и мы сделаем всё, чтобы выполнить ваш заказ качественно и оперативно! </p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>	
+						<div class="col-md-3 col-sm-4 col-xs-12">
+							<div class="services-sidebar">
+								<div class="sidebar-box sidebar-box_view">
+									<ul class="sidebar-menu">
+										<li class="sidebar-menu__item sidebar-menu__item_first"><a href="#" class="sidebar-menu__link">Индивидуальный заказ</a></li>
+										<li class="sidebar-menu__item sidebar-menu__item_first"><a href="#" class="sidebar-menu__link">Создать свой букет</a></li>
+										<li class="sidebar-menu__item sidebar-menu__item_first"><a href="#" class="sidebar-menu__link">Букет сезона</a></li>
+									</ul>
+									<span>СВАДЕБНАЯ ФЛОРИСТИКА</span>
+									<ul class="sidebar-menu sidebar-menu_margin">
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Букет невесты</a></li>
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Свадебное оформление</a></li>
+									</ul>
+								
+									<span class="alone">ФЛОРИСТИЧЕСКОЕ ОФОРМЛЕНИЕ</span>
+									<ul class="sidebar-menu">
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Праздники</a></li>
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Мероприятия</a></li>
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Интерьерные композиции</a></li>
+									</ul>
+								
+									<span>ТРАУРНАЯ ФЛОРИСТИКА</span>
+									<ul class="sidebar-menu">
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Траурные букеты</a></li>
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Венки из живых цветов</a></li>
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Оформление</a></li>
+									</ul>
+								
+									<span>КОМНАТНЫЕ РАСТЕНИЯ</span>
+									<ul class="sidebar-menu">
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Озеленение помещений</a></li>
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Уход</a></li>
+										<li class="sidebar-menu__item"><a href="#" class="sidebar-menu__link">Композиции из комнатных<br>&nbsp;&nbsp;&nbsp;растений</a></li>
+									</ul>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="n-footer">
+		<?php require_once('includes/footer_top.php'); ?>
+		<?php require_once('includes/footer_bottom.php'); ?>
+	</div>
+</div>
+</body>
+</html>
+
+
+
