efe797c626953bd69f26a76f05a8e5782a75da43
engine77 <zaur.1kager@gmail.com>
2017-05-09 12:14:13 +0400

    all file

--- cart1.php
+++ cart1.php
@@ -23,10 +23,81 @@
 						
 
 						<div class="cart__left_block">
-							
+							<div class="cart__left_block__img">
+								<img src="img/sliderbig1.jpg" alt="">
+							</div>
+							<div class="cart__slider js-owl-slidercart">
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/slidercart1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+								<div class="item">
+									<div class="cart__slider_img">
+										<img src="img/slidercart1.jpg" data-bigimg="img/sliderbig1.jpg" alt="">
+									</div>
+								</div>
+							</div>
 						</div>
 
+						<div class="cart__right_block">
+							
+							<div class="cart__compound">
+								<span class="cart__compoundtxt">Состав:</span>
+								<table class="cart__compound__table">
+									<tr>
+										<td>розы</td>
+										<td>—  21 шт</td>
+									</tr>
+									<tr>
+										<td>хризантемы</td>
+										<td>—  5 шт</td>
+									</tr>
+									<tr>
+										<td>гербера</td>
+										<td>—  27 шт</td>
+									</tr>
+								</table>
+							</div>
+							<div class="cart__characteristics">
+								<div class="cart__char__left">
+									<span>Размер:</span>
+									<span>Артикул:</span>
+									<span>Кол-во:</span>
+									<span>Цвет:</span>
+								</div>
+								<div class="cart__char__right">
+									<span>20 cм <b>40 cм</b></span>
+									<span>12 34567</span>
+									<div class="cart__char__right__number">
+										<span class="cart-button cart-button2">-</span>
+										<input type="text" value="1" disabled="disabled" id="x">  
+										<span class="cart-button cart-button3">+</span>
+									</div>
+									<div class="cart__char__right__colors">
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+									</div>
+								</div>
+							</div>
 
+
+
+						</div>
 
 
 
--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,70 @@
 jQuery(document).ready(function() {
+
+	$('.cart__slider_img img').on('click',function(evt) { 
+		evt.preventDefault();       		
+		var imgPath = $(this).attr('data-bigimg');         
+		var oldImage = $('.cart__left_block__img img');              
+		var newImage = $('<img src="' + imgPath +'">' ); 		
+		$(this).parents('.cart__left_block').find('.cart__left_block__img').html(newImage); 
+	});
+
 	
+	var $owlitem2 = $('.js-owl-slidercart');
+	$owlitem2.trigger('destroy.owl.carousel');
+
+	$owlitem2.html($owlitem2.find('.owl-stage-outer').html()).removeClass('owl-loaded');
+	$owlitem2.owlCarousel({                           
+		loop:true,
+		nav:true, 
+		autoplay:false,
+		smartSpeed:1000,
+		margin:10,
+		mouseDrag:false,
+		touchDrag: false,
+		navText:['<span class="cart-left"></span>','<span class="cart-right"></span>'],
+		responsive:{
+			0:{
+				items:1
+			},
+			410:{
+				items: 2   
+			},        
+			700:{
+				items:3
+			},
+			1000:{
+				items:4
+			}
+		}
+
+	}); 
 
 
 
-	
+
+  $(".cart__char__right__number .cart-button").on("click", function() {
+  	var thisbutton = $(this);
+  	var oldValue = thisbutton.parent().find("input").val();
+  	oldValue =  parseFloat(oldValue);
+  	if (thisbutton.text() == "+") {
+  		var newVal = oldValue + 1;   		
+  	}  
+  	else {   
+  		if (oldValue > 1) {
+  			var newVal = oldValue - 1;       
+  		} else {
+  			newVal = 1;      
+  		}
+  	}
+ thisbutton.parents('.cart__char__right__number').find("input").val(newVal);
+});
+
+
+
+$(".dfopen1 a").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".dfimge1").offset().top - 170
+    }, 500);
+});
+
 });
