4990c15c1d1b95171ae28c2b26d1e9b887029c37
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-09 23:46:44 +0600

    registration begin

---
+++ registration.php
@@ -0,0 +1,50 @@
+<?php require_once('includes/header.php'); ?>
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+			<div class="inner-page">
+				<div class="container">
+					<div class="row">
+						<div class="inner-page-top clearfix">
+							<div class="col-md-12">
+								<div class="breadcrumps-wrap">
+									<ul class="breadcrumps">
+										<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg"></a><i>/</i></li>
+										<li class="breadcrumps__link"><a href="#">Цветы в коробках</a><i>/</i></li>
+										<li class="breadcrumps__link"><span>О нас</span></li>
+									</ul>
+								</div>
+								<div class="h1-wrap">
+									<h1 class="h1">Регистрация</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="registration">
+						<div class="registration__login">
+							<div class="login__link">
+								<span>Вы уже зарегистрировались?</span>
+								<a href="#">Войти</a>
+							</div>
+							<div class="login__link">
+								<a href="#">Восстановление пароля</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="n-footer">
+		<?php require_once('includes/footer_top.php'); ?>
+		<?php require_once('includes/footer_bottom.php'); ?>
+	</div>
+</div>
+</body>
+</html>
+
+
+
