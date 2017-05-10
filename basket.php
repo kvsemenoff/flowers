bade268361af7939a18857434b9968d171aa5531
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-10 18:14:56 +0600

    busket1 ready

--- basket.php
+++ basket.php
@@ -18,57 +18,114 @@
 									</ul>
 								</div>
 								<div class="h1-wrap">
-									<h1 class="h1">Регистрация</h1>
+									<h1 class="h1">Оформление заказа</h1>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="registration">
-						<div class="registration__login">
-							<div class="login__link">
-								<span>Вы уже зарегистрировались?</span>
-								<a href="#">Войти</a>
-							</div>
-							<div class="login__link">
-								<a href="#">Восстановление пароля</a>
-							</div>
+					<div class="busket">
+						<div class="busket__nav-wrap clearfix">
+							<span class="busket__nav busket__nav_active">Корзина</span>
+							<span class="busket__nav">Доставка</span>
+							<span class="busket__nav">Ваши контакты</span>
+							<span class="busket__nav">Оплата</span>
 						</div>
-						<div class="registration-form">
-							<form action="#" id="formregist">
-							  <div class="input-wrap clearfix">
-							  	<label class="input__label" for="uname">Имя</label>
-							    <input id="uname" class="input__control" type="text" name="uname" required>
-							  </div>
-							  <div class="input-wrap clearfix">
-							  	<label class="input__label" for="second-name">Фамилия</label>
-							    <input class="input__control" type="text" name="second-name" id="second-name">
-							  </div>
-							  <div class="input-wrap clearfix">
-							  	<label class="input__label" for="patronymic">Отчество</label>
-							    <input class="input__control" type="text" name="patronymic" id="patronymic">
-							  </div>
-							  <div class="input-wrap clearfix">
-							  	<label class="input__label" for="mail">E-mail</label>
-							    <input class="input__control" type="email" name="mail" id="mail" required>
-							  </div>
-							  <div class="input-wrap clearfix">
-							  	<label class="input__label" for="password">Пароль</label>
-							    <input class="input__control" type="password" name="password" id="password" required>
-							  </div>
-							  <div class="input-wrap clearfix">
-							  	<label class="input__label" for="r_password">Повторите пароль</label>
-							    <input class="input__control" type="password" name="r_password" id="r_password" required>
-							  </div>
-							  <div class="input-wrap input-wrap_button">
-							    <label class="checkbox">
-										<input class="checkbox__hidden" type="checkbox" name="checkbox-test" checked="checked">
-										<span class="checkbox__custom"></span>
-										<span class="checkbox__label"> Я согласен на использование моих данных и с <a href="#">политикой конфиденциальности</a></span>
-									</label>
-									<span class="form__descr"><i>* </i>Поля обязательные для заполнения</span>
-									<button class="button button_main">Зарегистрироваться</button>
-							  </div>
-							</form>
+						<div class="busket__tovar-wrap">
+							<div class="h4-wrap">
+								<h4 class="h4">Корзина</h4>
+							</div>
+							<div class="busket__tovar-table">
+								<div class="busket__tovar-row">
+									<div class="busket__tovar clearfix">
+										<div class="tovar__info tovar__info_view">
+											<span class="tovar__descr">Товар</span>
+											<div class="tovar__cart clearfix">
+												<div class="tovar__img">
+													<img src="img/flower1.jpg" alt="">
+												</div>
+												<div class="tovar__title">
+													<a href="#" class="tovar__name">Облачная Симфония</a>
+													<span class="tovar__kind">Стандартный</span>
+												</div>
+											</div>
+										</div>
+										<div class="tovar__info tovar__info_quan">
+											<span class="tovar__descr">Количество</span>
+											<div class="cart__char__right__number">
+												<span class="cart-button cart-button2">-</span>
+												<input type="text" value="1" disabled="disabled" id="x">  
+												<span class="cart-button cart-button3">+</span>
+											</div>
+										</div>
+										<div class="tovar__info tovar__info_single">
+											<span class="tovar__descr">Цена за единицу</span>
+											<span class="tovar__price">1 200 руб.</span>
+										</div>
+										<div class="tovar__info tovar__info_summ">
+											<span class="tovar__descr">Сумма</span>
+											<span class="tovar__price">2 400 руб.</span>
+										</div>
+										<div class="tovar__info tovar__info_delete">
+											<span class="tovar__descr">/</span>
+											<a href="#" class="tovar__delete">Удалить</a>
+										</div>
+									</div>
+								</div>
+								<div class="busket__tovar-row">
+									<div class="busket__tovar clearfix">
+										<div class="tovar__info tovar__info_view">
+											<span class="tovar__descr">Товар</span>
+											<div class="tovar__cart clearfix">
+												<div class="tovar__img">
+													<img src="img/flower1.jpg" alt="">
+												</div>
+												<div class="tovar__title">
+													<a href="#" class="tovar__name">Облачная Симфония</a>
+													<span class="tovar__kind">Стандартный</span>
+												</div>
+											</div>
+										</div>
+										<div class="tovar__info tovar__info_quan">
+											<span class="tovar__descr">Количество</span>
+											<div class="cart__char__right__number">
+												<span class="cart-button cart-button2">-</span>
+												<input type="text" value="1" disabled="disabled" id="x">  
+												<span class="cart-button cart-button3">+</span>
+											</div>
+										</div>
+										<div class="tovar__info tovar__info_single">
+											<span class="tovar__descr">Цена за единицу</span>
+											<span class="tovar__price">1 200 руб.</span>
+										</div>
+										<div class="tovar__info tovar__info_summ">
+											<span class="tovar__descr">Сумма</span>
+											<span class="tovar__price">2 400 руб.</span>
+										</div>
+										<div class="tovar__info tovar__info_delete">
+											<span class="tovar__descr">/</span>
+											<a href="#" class="tovar__delete">Удалить</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="busket__order clearfix">
+								<div class="order__discount">
+									<div class="discount-wrap">
+										<span class="discount__title">У вас есть дисконтная карта постоянного покупателя*?</span>
+										<div class="input input_theme_rosa">
+											<input type="text" class="input__control" name="discount" placeholder="Введите номер вашей карты" required>
+										</div> 
+									</div>
+									<p class="order__discount-info">**Постоянным клиентам выдаются дисконтные карты в салонах, при оформлении заказа.</p>
+									<p class="order__discount-info">Этот текст ненастоящий, нужно все подробнее узнать у заказчика</p>
+								</div>
+								<div class="order__price">
+									<p class="order__price-title">Вы сформировали заказ на сумму: <i>4 800 руб.</i></p>
+									<div class="button-wrap">
+										<a href="#" class="button button_main button_mini">Оформить заказ</a>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
