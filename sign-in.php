c4789306d61fdefe2efc2087b2f65a95414e8308
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-12 01:47:29 +0600

    start sign-in

---
+++ sign-in.php
@@ -0,0 +1,77 @@
+<?php require_once('includes/header.php'); ?>
+<div class="n-wrapper">
+	<div class="n-header">
+		<?php require_once('includes/header-top.php'); ?>
+	</div>
+	<div class="n-content">
+		<div class="n-content-inner vertical-align">
+			<div class="container">
+				<div class="row">
+					<div class="cart-inner">
+						<div class="breadcrumps-wrap">
+							<ul class="breadcrumps">
+								<li class="breadcrumps__link breadcrumps__link_home"><a href="/" class="bread_menu__link_bg"></a><i>/</i></li>
+								<li class="breadcrumps__link"><a href="#">Цветы в коробках</a><i>/</i></li>
+								<li class="breadcrumps__link"><a href="#">Корзина</a><i>/</i></li>
+								<li class="breadcrumps__link"><span>Букет из хризантем</span></li>
+							</ul>
+						</div>
+						<div class="h1-wrap">
+							<h1 class="h1">Личный кабинет</h1>
+						</div>											
+						<!-- part -2 -->
+						<div class="review-block">
+							<div class="tabs-block">
+					      <div class="tabs">
+					        <div class="tab tab-0 active">История заказов</div>
+					        <div class="tab tab-1">Личные данные</div>
+					      </div>
+					      <div class="tabs-content">
+					        <div class="tab-content tab-0">
+					          <div class="tab-content__inner">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Злых толку ручеек запятой продолжил великий приставка, прямо свою дорогу переулка ему продолжил щеке, маленький, языком ее своего буквенных речью?</div>
+					        </div>
+					      </div>
+					      <div class="tabs-content">
+					        <div class="tab-content tab-1">
+						        <div class="tab-content__inner clearfix">
+						          <div class="tab-content__title">
+						          	<span class="review__name">Татьяна Иванова</span>
+						          	<span class="review__date">21.02.2017</span>
+						          </div>
+						          <div class="tab-content__text">
+						          	<p>Рыбные тексты также применяются для демонстрации различных видов шрифта и в разработке макетов. Как правило их содержание бессмысленно. По причине своей функции текста-заполнителя для макетов нечитабельность рыбных текстов имеет особое значение, так как человеческое восприятие имеет особенность, распознавать определенные образцы и повторения.</p>
+						          	<div class="review__raitings">
+						          		<div class="review__raiting review__raiting_like">
+						          			<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+						          			<i class="like-quant">0</i>
+					          			</div>
+						          		<div class="review__raiting review__raiting_dislike">
+						          			<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+						          			<i class="dislike-quant">0</i>
+						          		</div>
+						          	</div>
+						          </div>
+						        </div>
+					        </div>
+					      </div>
+					    </div>
+					    <div class="button-wrap">
+								<a href="#" class="button button_main button_review">Оставьте отзыв о товаре</a>
+					    	<a href="#" class="all-review"><span>Все отзывы (4)</span><i class="fa fa-chevron-down" aria-hidden="true"></i></a>
+					    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="n-footer">
+		<?php require_once('includes/footer_top.php'); ?>
+		<?php require_once('includes/footer_bottom.php'); ?>
+	</div>
+</div>
+</body>
+</html>
+
+
+
